c06e7066e45b1c6263a0952b54c1d8ef762cb6ba
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-09-02 18:51:08 +0000

    Added del function

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app     = express();
 const PORT    = 8080; //default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.set("view engine", "ejs");
@@ -28,15 +29,30 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls/:shortURL", (req, res) => {
+/*app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: "http://www.lighthouselabs.ca"};
   res.render("urls_show", templateVars);
-});
+});*/
 
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = "http://www.lighthouselabs.ca";
+  res.redirect(longURL);
+});
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.delete('/urls/:shortURL/delete', (req, res) => {
+  const shortURL = req.params.shortURL;
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
