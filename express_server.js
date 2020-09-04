99ea07ba2e9822072f41212e101ac3ece86d9262
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-09-04 23:33:41 +0000

    Modified till login/out function

--- express_server.js
+++ express_server.js
@@ -3,13 +3,32 @@ const app     = express();
 const PORT    = 8080; //default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-
-
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 app.set("view engine", "ejs");
+const { emailLookUp} = require('./helpers');
+//const bcrypt = require('bcrypt');
+
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "userRandomID" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJurls48lW" }
+};
+
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+const generateRandomString = () => {
+  return Math.random().toString(36).substring(2,8);
 };
 
 
@@ -26,13 +45,25 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
+  if (!req.cookies.user_id) {
   res.render("urls_new");
+}
+let templateVars = { user: users[req.cookies.user_id] };
+  res.render("urls_new", templateVars);
 });
 
 /*app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: "http://www.lighthouselabs.ca"};
   res.render("urls_show", templateVars);
 });*/
+
+app.get("/login", (req, res) => {
+  var user = {};
+  const userId = req.cookies.user_id;
+  console.log(userId);
+  let templateVars = { user: users[userId] };
+  res.render("login", templateVars);
+});
 
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
@@ -41,9 +72,85 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  var user = {};
+  const userId = req.cookies.user_id;
+  console.log(userId);
+  if(userId)
+  {
+    user =  users[userId];
+   }
+  let templateVars = { user: user, urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get('/register', (req, res) => {
+  const userId = req.cookies.user_id;
+  if (users[userId]) {
+    res.redirect('/urls');
+  }else{
+    let templateVars = {user: users[userId]};
+    res.render("urls_registration", templateVars);
+  }
+});
+
+app.post('/register', (req, res) => {
+  if(!req.body.email && !req.body.password)  {
+    res.status(400).send("Invalid details");
+    return;
+  } else if(emailLookUp(req.body.email, users)) {
+    res.status(400).send("User exists!");
+    return;
+  } else {
+    let newUser = generateRandomString();
+    users[newUser] = {
+      id: newUser,
+      email: req.body.email,
+      password: req.body.password
+    };
+    res.cookie('user_id', newUser);
+    res.redirect('/urls');
+  }
+});
+
+app.post("/login", (req, res) => {
+  const email    = req.body.email;  
+  const password = req.body.password;
+  const user = emailLookUp(email, users);
+  console.log("Testing the user", user);
+  if (user) {
+    console.log("Testing user.password",user.password);
+    if (password === user.password) {
+      res.cookie('user_id', user.id);
+      console.log("hello");
+      return res.redirect("/urls");
+    } else {
+      return res.status(403).send("Wrong Password");
+    }
+  } else {
+    res.status(403).send("User Not Found");
+  }
+  });
+  
+
+
+app.post("/logout", (req, res) => {
+  res.cookie('user_id', "");
+  res.redirect("/urls");
+});
+
+
+
+
+// GET '/' sends logged in users to the '/urls' page and logged out users to the '/login' page
+/*app.get('/', (req, res) => {
+  const userId = req.session.user_id;
+  if (userId) {
+    res.redirect('/urls');
+  } else {
+    res.redirect('/login');
+  }
+});*/
+
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
